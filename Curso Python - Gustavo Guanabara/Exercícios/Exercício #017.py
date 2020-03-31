ab3207f871d30be559b0e702b132d36212a7d312
sarandrade <sarandradedias@gmail.com>
2020-03-31 14:36:35 -0300

    Exercício 17

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #017.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #017.py
@@ -1,1 +1,8 @@
 # Desafios 17
+
+import math
+
+co = float(input('Digite o cateto oposto: '))
+ca = float(input('Digite o cateto adjacente: '))
+
+print('A hipotenusa é {}'.format(math.hypot(co, ca)))
