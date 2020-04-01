42c11df05a0cd474254036a5628e60535f9affd9
sarandrade <sarandradedias@gmail.com>
2020-04-01 16:27:50 -0300

    Aula 12

---
+++ Curso Python - Gustavo Guanabara/Aulas/Aula #12 - Condições Aninhadas.py
@@ -0,0 +1,23 @@
+# Aula #12 - Condições Aninhadas
+
+'''
+- Estrutura Condicional Aninhada
+
+if condição_1:
+    função_1
+elif condição_2:
+    função_2
+else:
+    função_3
+'''
+nome = input('Qual seu nome? ')
+
+if nome == 'Sara':
+    print('Que nome lindo!')
+elif nome == 'Túlio':
+    print('Que nome diferente.')
+elif nome in 'Mari Paula Pedro':
+    print('Muito popular o seu nome!')
+else:
+    print('Nome legal!')
+print('Bom dia, {}.'.format(nome))
