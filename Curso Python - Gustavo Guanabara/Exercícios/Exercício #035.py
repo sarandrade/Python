da4852c9d7ca4e0042b9cfc7280416ed18850ca9
sarandrade <sarandradedias@gmail.com>
2020-04-01 11:46:49 -0300

    Exercício 35

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #035.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #035.py
@@ -1,1 +1,12 @@
 # Desafios 35
+
+lista = input('Digite os valores dos três lados: ').split() 
+
+r1 = float(lista[0])
+r2 = float(lista[1])
+r3 = float(lista[2])
+
+if (r1 > (r2 + r3) or r2 > (r1 + r3) or r3 > (r1 + r2)):
+    print('Não é possível for um triângulo com esses lados.')
+else:
+    print('É possível for um triângulo com esses lados.')
