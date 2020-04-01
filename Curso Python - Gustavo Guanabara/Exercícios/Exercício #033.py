ea88005e880e16cd0477cfd419220594b423d029
sarandrade <sarandradedias@gmail.com>
2020-04-01 11:26:10 -0300

    Exercício 33

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #033.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #033.py
@@ -1,2 +1,17 @@
 # Desafios 33
 
+a, b, c = input('Digite três números diferentes: ').split()
+
+if (a > b and a > c):
+    print('{} é o maior número.'.format(a))
+elif b > c:
+    print('{} é o maior número.'.format(b))
+else:
+    print('{} é o maior número.'.format(c))
+
+if (a < b and a < c):
+    print('{} é o menor número.'.format(a))
+elif b < c:
+    print('{} é o menor número.'.format(b))
+else:
+    print('{} é o menor número.'.format(c))
