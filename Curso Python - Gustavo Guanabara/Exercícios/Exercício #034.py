fcc89eaa5fed521fdcd57c0b1de189407155e6ef
sarandrade <sarandradedias@gmail.com>
2020-04-01 11:30:31 -0300

    Exercício 34

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #034.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #034.py
@@ -1,1 +1,8 @@
 # Desafios 34
+
+sal = float(input('Digite seu salário: '))
+
+if sal <= 1250:
+    print('Se salário com o aumento será R${:.2f}.'.format(sal * 1.15))
+else:
+    print('Se salário com o aumento será R${:.2f}.'.format(sal * 1.1))
