baa02c7d1821d9b916d089df82452fcd30eb2497
sarandrade <sarandradedias@gmail.com>
2020-04-01 10:50:37 -0300

    Exercício 29

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #029.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #029.py
@@ -1,2 +1,10 @@
 # Desafios 29
 
+vel = float(input('Qual a sua velocidade em Km/h? '))
+
+if vel > 80:
+    print('Você foi multado! Sua multa custa R${:.2f}'.format((vel-80) * 7))
+elif vel == 80:
+    print('Cuidado!! Você está no limite de velocidade.')
+else: 
+    print('Abaixo do limite de velocidade.')    
