bfb2a117818b14a7516c8749f679dff0a9ccac3a
sarandrade <sarandradedias@gmail.com>
2020-04-02 19:23:57 -0300

    Exercício 42

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #042.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #042.py
@@ -1,1 +1,19 @@
 # Desafios 42
+
+r1 = float(input('Digite o valor do primeiro lado: '))
+r2 = float(input('Digite o valor do segundo lado: '))
+r3 = float(input('Digite o valor do terceiro lado: '))
+
+if r1 < (r2 + r3) and r2 < (r1 + r3) and r3 < (r1 + r2):
+    
+    print('Os lados escolhidos formam um triângulo.')
+    
+    if r1 == r2 and r1 == r3:
+        print('Triângulo Equilátero.')
+    elif r1 != r2 and r2 != r3 and r1 != r3:
+        print('Triângulo Escaleno.')
+    else: 
+        print('Isósceles.')
+
+else:
+    print('Os lados escolhidos NÃO formam um triângulo.')
