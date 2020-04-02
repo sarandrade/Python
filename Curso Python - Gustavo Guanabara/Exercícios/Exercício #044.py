d6244e285462c6d81e9241f1916d0306481ac34f
sarandrade <sarandradedias@gmail.com>
2020-04-02 19:43:07 -0300

    Exercício 44

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #044.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #044.py
@@ -1,1 +1,26 @@
 # Desafios 44
+
+valor = float(input('Digite o valor do produto: '))
+
+print('''Escolha a opção de pagamento:
+[ 1 ] Dinheiro / Cheque.
+[ 2 ] À vista no Cartão. 
+[ 3 ] 2x no cartão.
+[ 4 ] 3x ou mais no cartão.''')
+
+opcao = int(input('Digite a sua opção: '))
+
+if opcao == 1:
+    preco = valor * 0.9
+    print('O valor do produto será R${:.2f}.'.format(preco))
+elif opcao == 2:
+    preco = valor * 0.95
+    print('O valor do produto será R${:.2f}.'.format(preco))
+elif opcao == 3:
+    preco = valor 
+    print('O valor do produto será R${:.2f}.'.format(preco))
+elif opcao == 4:
+    preco = valor * 1.2
+    print('O valor do produto será R${:.2f}.'.format(preco))
+else: 
+    print('Opção inválida!')
