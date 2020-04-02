896121b81d2927476cf9e7a09cb8d220d7525f05
sarandrade <sarandradedias@gmail.com>
2020-04-02 19:32:14 -0300

    Exercício 43

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #043.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #043.py
@@ -1,1 +1,17 @@
 # Desafios 43
+
+peso = float(input('Digite seu peso em Kg: '))
+altura = float(input('Digite sua altura em m²: '))
+
+imc = peso / altura
+
+if imc <= 18.5:
+    print('Abaixo do peso.')
+elif imc <= 25:
+    print('Peso ideal.')
+elif imc <= 30:
+    print('Sobrepeso.')
+elif imc <= 40:
+    print('Obeso.')
+else: 
+    print('Obesidade Mórbida.') 
