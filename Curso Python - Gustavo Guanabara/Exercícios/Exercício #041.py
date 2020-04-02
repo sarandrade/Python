d1e80c652ab0a3e253a9b9dbfc93e65dc87cd820
sarandrade <sarandradedias@gmail.com>
2020-04-02 14:58:15 -0300

    Exercício 41

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #041.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #041.py
@@ -1,1 +1,24 @@
 # Desafios 41
+
+from datetime import date
+
+nasc = int(input('Digite o ano de seu nascimento: '))
+
+hoje = date.today().year
+
+idade = hoje - nasc
+
+print('Você tem {:.0f} anos.'.format(idade))
+
+if idade <= 9:
+    categ = 'Mirin'
+elif idade <= 14:
+    categ = 'Infantil'
+elif idade <= 19:
+    categ = 'Junior'
+elif idade <= 20: 
+    categ = 'Sênior'
+else:
+    categ = 'Master'
+
+print('Logo, sua categoria é -> {}.'.format(categ))
