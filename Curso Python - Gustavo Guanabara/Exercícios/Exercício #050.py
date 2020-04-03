b5e5c72a49b46d51de9768f6619d3b7d77daac29
sarandrade <sarandradedias@gmail.com>
2020-04-03 13:47:03 -0300

    Exercício 50

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #050.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #050.py
@@ -1,1 +1,11 @@
 # Desafios 50
+
+soma = 0
+
+for i in range(0, 6):
+    num = float(input('Digite um número: '))
+
+    if num % 2 == 0:
+        soma += num
+
+print('A soma dos números pares digitados é igual a {}.'.format(soma))
