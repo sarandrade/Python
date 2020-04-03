6ebcc1f00b14961c3e6c0f81c92ccb3873d4187d
sarandrade <sarandradedias@gmail.com>
2020-04-03 14:07:11 -0300

    Exercício 51

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #046.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #046.py
@@ -4,6 +4,7 @@ from time import sleep
 import emoji
 
 for i in range(10, -1, -1):
-    print(' -+- {} -+-'.format(i))
+    print(' -+- {:2} -+-'.format(i))
+    sleep(1)
 
 print(emoji.emojize('\nFELIZ ANO NOVO!!:sparkler:\n'))
--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #051.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #051.py
@@ -1,1 +1,7 @@
 # Desafios 51
+
+inicio = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+
+for i in range(inicio, inicio + 10 * razao, razao):
+    print(i, end=' ')
