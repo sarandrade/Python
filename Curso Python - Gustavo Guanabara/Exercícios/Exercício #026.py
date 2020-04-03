0e3296b749a75d6ab9bad0e2ea508c107e08ff5f
sarandrade <sarandradedias@gmail.com>
2020-04-03 14:53:40 -0300

    Exercício 53

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #026.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #026.py
@@ -1,7 +1,11 @@
 # Desafios 26
 
 frase = input('Digite uma frase: ').upper().strip()
+
 num = frase.count('A')
+
 print('A letra A aparece {} vezes. '.format(frase.count('A')))
+
 print('A primeira letra A apareceu na posição {}.'.format(frase.find('A') + 1))
+
 print('A última letra A apareceu na posição {}.'.format(frase.rfind('A') + 1))
--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #053.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #053.py
@@ -1,1 +1,23 @@
 # Desafios 53
+
+from math import trunc
+
+frase = input('Digite a frase: ').strip().upper()
+
+tamanho = len(frase)
+
+x = 0
+
+for i in range(0, trunc(tamanho/2) + 1):
+    inicio = i
+    fim = tamanho - i - 1
+
+    if frase[i] != frase[fim]: 
+        x += i # A frase não é um políndromo
+    else:
+        x = 0
+
+if x > 0:
+    print('A frase não é um políndromo.')    
+else:
+    print('A frase é um políndromo.')
