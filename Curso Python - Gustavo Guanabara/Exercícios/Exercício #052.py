e58c2595814185f7b7e2c5d64b410cdc9991cc47
sarandrade <sarandradedias@gmail.com>
2020-04-03 14:27:14 -0300

    Exercício 52

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #052.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #052.py
@@ -1,1 +1,21 @@
 # Desafios 52
+
+from math import trunc
+
+num = int(input('Digite um número inteiro: '))
+
+limite = trunc(num ** (1/2))
+
+soma = 0
+
+for i in range(2, limite + 1):
+
+    if num % i == 0:
+        print('{} é divisível por {}.'.format(num, i))
+        soma += i
+
+if soma == 0:
+    print('O número é divisível apenas por 1 e por ele mesmo.')
+    print('Logo, {} é um número primo!!'.format(num))
+else:
+    print('{} não é um número primo.'.format(num))
