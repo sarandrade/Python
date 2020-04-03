1fce8ff150d0911a3d2324f9630f2e7284455711
sarandrade <sarandradedias@gmail.com>
2020-04-03 13:42:36 -0300

    Exercício 49

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #049.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #049.py
@@ -1,1 +1,8 @@
 # Desafios 49
+
+n = int(input('Digite um número: '))
+
+print('  {} TABUADA DE {} {}'.format('-+-', n, '-+-'))
+
+for i in range(0, 11):
+    print('[ {:2} ] X [ {} ] = [ {:2} ]'.format(i, n, i * n))
