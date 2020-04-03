4175cdc0e12da766ec0db13af8a6711306403d35
sarandrade <sarandradedias@gmail.com>
2020-04-03 12:32:18 -0300

    Exercício 48

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #048.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #048.py
@@ -1,1 +1,10 @@
 # Desafios 48
+
+soma = 0
+
+for i in range(1, 500, 2):
+    
+    if i % 3 == 0:
+        soma += i
+
+print('A soma de todos os números ímpares múltiplos de 3 entre 1 e 500 é {}.'.format(soma))
