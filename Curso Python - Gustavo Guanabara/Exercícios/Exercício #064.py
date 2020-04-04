83bf90079204300926fa9d9feaf189c4ee6b7cf6
sarandrade <sarandradedias@gmail.com>
2020-04-04 20:52:36 -0300

    Exercício 64

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #064.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #064.py
@@ -1,1 +1,14 @@
 # Desafios 64
+
+num = 0
+soma = 0
+i = 0
+
+while num != 999:
+    num = int(input('Digite um número inteiro: '))
+
+    if num != 999:
+        soma += num
+        i += 1
+
+print('A soma dos {} números digitados é {}.'.format(i, soma))
