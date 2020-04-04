73d4d324b25e85c9559d95da3ca98a47a0ed8380
sarandrade <sarandradedias@gmail.com>
2020-04-04 20:26:55 -0300

    Exercício 61

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #061.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #061.py
@@ -1,1 +1,21 @@
 # Desafios 61
+
+'''
+=> Utilizando a estrutura for: 
+
+inicio = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+
+for i in range(inicio, inicio + 10 * razao, razao):
+    print(i, end=' ')
+'''
+
+inicio = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+
+i = 10
+
+while i != 0:
+    print(inicio, end=' ')
+    inicio = inicio + razao
+    i -= 1
