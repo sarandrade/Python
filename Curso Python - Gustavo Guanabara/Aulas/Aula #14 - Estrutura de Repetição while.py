a946b4c060901408aa2927e93f5ee8c2e1fef4e9
sarandrade <sarandradedias@gmail.com>
2020-04-04 13:58:22 -0300

    Exercício 57

---
+++ Curso Python - Gustavo Guanabara/Aulas/Aula #14 - Estrutura de Repetição while.py
@@ -0,0 +1,28 @@
+# Aula #14 - Estrutura de Repetição while
+
+'''
+while condição:
+    comandos
+'''
+c = 1
+
+while c < 10: # Condição de parada
+    print(c)
+    c += 1
+
+par = impar = 0
+n = 1
+
+while n != 0:
+    n = int(input('Digite um número inteiro: '))
+
+    if n != 0:
+
+        if n % 2 == 0:
+            par += 1
+        else:
+            impar += 1
+
+print('Você digitou {} números pares e {} números ímpares.'.format(par, impar))
+
+# Desafios 57 -> 65
---
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #057.py
@@ -0,0 +1,8 @@
+# Desafios 57
+
+sexo = ''
+
+while not (sexo == 'M' or sexo == 'F'):
+    sexo = input('Digite o seu sexo [F/M]: ').upper().strip()
+
+print('Muito Obrigada!!')
