4a3d334976bc5f00b2dd3f042abb64d16f69d769
sarandrade <sarandradedias@gmail.com>
2020-04-04 20:44:04 -0300

    Exercício 63

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #063.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #063.py
@@ -1,1 +1,20 @@
 # Desafios 63
+
+num = int(input('Digite um número inteiro: '))
+
+print('0 -> 1 -> ', end='')
+
+seq1 = 0
+seq2 = 1
+
+while num != 2:
+    termo = seq1 + seq2
+
+    seq1 = seq2
+    seq2 = termo
+    num -= 1
+
+    if num == 2:
+        print('{}'.format(termo), end='')
+    else:
+        print('{} -> '.format(termo), end='')
