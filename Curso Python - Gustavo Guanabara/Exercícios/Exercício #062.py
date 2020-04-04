abdb6fe6c140f3537d51a610b3b0ebc7c3baf11e
sarandrade <sarandradedias@gmail.com>
2020-04-04 20:30:40 -0300

    Exercício 62

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #062.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #062.py
@@ -1,1 +1,15 @@
 # Desafios 62
+
+inicio = int(input('Digite o primeiro termo da PA: '))
+razao = int(input('Digite a razão da PA: '))
+
+i = 10
+
+while i != 0:
+    print(inicio, end=' ')
+    inicio = inicio + razao
+    i -= 1
+
+    if i == 0:
+        opcao = int(input('\nDeseja ver mais quantos termos? '))
+        i = opcao
