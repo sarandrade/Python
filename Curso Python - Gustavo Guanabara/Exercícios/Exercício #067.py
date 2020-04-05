ff55cb504d46ffd51f75d0ed6dca968f635b6db1
sarandrade <sarandradedias@gmail.com>
2020-04-05 10:07:43 -0300

    Exercício 67

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #067.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #067.py
@@ -1,1 +1,14 @@
 # Desafios 67
+
+while True:
+    num = int(input('Quer ver a tabuada de qual número? '))
+
+    if num < 0:
+        break
+
+    print('-'*25)
+    for i in range(1, 11):
+        print(f'[ {i:2} ] x [ {num} ] = [ {num*i:2} ]')
+    print('-'*25)
+
+print('Programa encerrado...')
