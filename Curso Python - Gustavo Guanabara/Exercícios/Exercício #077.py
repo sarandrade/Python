8e24e8ca89fe78145f281814ca7333d7dcd2b0b6
sarandrade <sarandradedias@gmail.com>
2020-04-06 13:52:23 -0300

    Exercício 77

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #077.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #077.py
@@ -1,1 +1,11 @@
 # Desafios 77
+
+tupla = ('Sara', 'Tulio', 'Mari', 'Paula', 'Hellen')
+
+for termo in tupla:
+    print(f'\n{termo} => ', end='')
+
+    for letra in termo:
+
+        if letra in 'AEIOUaeiou':
+            print(letra, end=' ')
