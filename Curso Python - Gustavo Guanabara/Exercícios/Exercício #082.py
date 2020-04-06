5bc3cabaf920e1caad10017b3180ed1c11f6efcc
sarandrade <sarandradedias@gmail.com>
2020-04-06 18:29:48 -0300

    Exercício 82

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #082.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #082.py
@@ -1,1 +1,27 @@
 # Desafios 82
+
+lista = []
+par = []
+impar = []
+
+while True:
+    num = int(input('Digite um valor: '))
+    lista.append(num)
+
+    opcao = ' '
+    while opcao not in 'SN':
+        opcao = input('Deseja continuar [S/N]? ').upper().strip()[0]
+    
+    if opcao == 'N':
+        break
+
+for i in lista:
+    if i % 2 == 0:
+        par.append(i)
+    else:
+        impar.append(i)
+
+print('-+-'*20)
+print(f'Lista completa: {lista}')
+print(f'Lista par: {par}')
+print(f'Lista ímpar: {impar}')
