0fbf1d8f9305dd4702c9a1163fd4fdc57fa4c8bc
sarandrade <sarandradedias@gmail.com>
2020-04-06 12:22:44 -0300

    Correção Ex. 73

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #073.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #073.py
@@ -3,7 +3,7 @@
 times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo')
 
 # A
-print(f'\nOs 5 primeiros times são: {times[0:6]}')
+print(f'\nOs 5 primeiros times são: {times[0:5]}')
 
 # B
 print(f'\nOs últimos 4 colocados são: {times[-4:]}')
@@ -14,3 +14,6 @@ print(f'\nEm ordem alfabética temos: {sorted(times)}')
 # D
 posicao = times.index('Chapecoense')
 print(f'\nO Chapecoense está na posição: {posicao + 1}')
+
+# D => Ou 
+print(f'\nO Chapecoense está na posição: {times.index("Chapecoense") + 1}')
