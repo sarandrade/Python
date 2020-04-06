032de3a92bfbe03dad7249196d0e22b75b1b9983
sarandrade <sarandradedias@gmail.com>
2020-04-06 12:16:00 -0300

    Exercício 74

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #074.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #074.py
@@ -1,1 +1,15 @@
 # Desafios 74
+
+from random import randint
+
+a = randint(0, 10)
+b = randint(0, 10)
+c = randint(0, 10)
+d = randint(0, 10)
+e = randint(0, 10)
+
+tupla = (a, b, c, d, e)
+
+print(f'Os valores sorteados foram: {tupla}')
+print(f'O maior valor sorteado foi: {sorted(tupla)[4]}')
+print(f'O menor valor sorteado foi: {sorted(tupla)[0]}')
