74fad162fc0a2d197dfce00857155b72e8af9061
sarandrade <sarandradedias@gmail.com>
2020-04-06 18:43:23 -0300

    Exercício 83

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #083.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #083.py
@@ -1,1 +1,16 @@
 # Desafios 83
+
+exp = input('Digite uma expressão: ')
+lista = list(exp)
+
+paren1 = paren2 = 0
+for i in lista:
+    if i == '(':
+        paren1 += 1
+    elif i == ')':
+        paren2 += 1
+
+if paren1 == paren2:
+    print('Expressão Válida!')
+else:
+    print('Expressão Inválida!')
