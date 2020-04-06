2abd5c4d3aba84f6e4279e27a87657eeffa148da
sarandrade <sarandradedias@gmail.com>
2020-04-06 12:04:36 -0300

    Exercício 73

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #073.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #073.py
@@ -1,1 +1,16 @@
 # Desafios 73
+
+times = ('Corinthias', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo')
+
+# A
+print(f'\nOs 5 primeiros times são: {times[0:6]}')
+
+# B
+print(f'\nOs últimos 4 colocados são: {times[-4:]}')
+
+# C
+print(f'\nEm ordem alfabética temos: {sorted(times)}')
+
+# D
+posicao = times.index('Chapecoense')
+print(f'\nO Chapecoense está na posição: {posicao + 1}')
