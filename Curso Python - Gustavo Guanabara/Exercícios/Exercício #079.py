be0babb09fbbea399a4d43eb1ccafd31b8878ed6
sarandrade <sarandradedias@gmail.com>
2020-04-06 15:34:11 -0300

    Exercício 79

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #079.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #079.py
@@ -1,1 +1,23 @@
 # Desafios 79
+
+lista = []
+
+while True:
+    num = int(input('Digite um número: '))
+
+    if num in lista:
+        print('Valor duplicado... \nNão vou adicionar...')
+    else:
+        lista.append(num)
+        print('Valor adicionado!!')
+
+
+    opcao = ' '
+    while opcao not in 'SN':
+        opcao = input('Quer continuar [S/N]? ').upper().strip()[0]
+    
+    if opcao == 'N':
+        break
+
+print('-+-'*20)
+print(f'Você digitou os valores: {sorted(lista)}')
