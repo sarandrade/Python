5d7983de0521b8788992d5e0047c73e489475a25
sarandrade <sarandradedias@gmail.com>
2020-04-06 13:36:57 -0300

    Exercício 76

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #076.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #076.py
@@ -1,1 +1,15 @@
 # Desafios 76
+
+print('-'*30)
+print(f'{"LISTA DE PREÇOS":^30}')
+print('-'*30)
+
+tupla = ('Caneta', 2, 'Caderno', 8, 'Livro', 20, 'Estojo', 15, 'Régua', 7.80)
+
+for posicao, termo in enumerate(tupla):
+    if posicao % 2 == 0:
+        print(f'{termo:.<20}', end='R$ ')
+    else:
+        print(f'{termo:>7.2f}')
+
+print('-'*30)
