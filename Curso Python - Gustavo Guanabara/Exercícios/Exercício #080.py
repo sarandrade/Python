13d4b9caf4feccc310f88b27c18b18ff6556fc82
sarandrade <sarandradedias@gmail.com>
2020-04-06 18:21:40 -0300

    Exercício 81

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #080.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #080.py
@@ -1,1 +1,30 @@
 # Desafios 80
+
+lista = []
+
+for i in range(0,5):
+    num = int(input(f'Digite um valor: '))
+    
+    if i == 0:
+        maior = num
+        lista.append(num)
+        print('O primeiro termo de sua lista foi adicionado...')
+    else:
+        if num > maior:
+            lista.append(num)
+            maior = num
+            print('O termo foi adicionado ao fim da lista...')
+        else: 
+            for posicao, termo in enumerate(lista):
+                if posicao == 0:
+                    if num < termo:
+                        lista.insert(0, num)
+                        print('O termo foi adicionado na posição 0...')
+                print(posicao)
+                print(termo)
+                print(lista[posicao])
+                if termo < num < lista[posicao + 1]:
+                    lista.insert(posicao, num)
+                    print(f'O termo foi adicionado na posição {posicao}...')
+
+print(lista)
--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #081.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #081.py
@@ -1,1 +1,29 @@
 # Desafios 81
+
+lista = []
+
+while True:
+    num = int(input('Digite um valor: '))
+    lista.append(num)
+
+    opcao = ' '
+    while opcao not in 'SN':
+        opcao = input('Deseja continuar [S/N]? ').upper().strip()[0]
+    
+    if opcao == 'N':
+        break
+
+print('-+-'*20)
+
+print(f'{len(lista)} valores foram digitados.')
+
+lista.sort(reverse=True)
+print(f'Em ordem decrescente: {lista}')
+
+if 5 in lista:
+    print('O valor 5 se encontra na posição:', end=' ')
+    for posicao, termo in enumerate(lista):
+        if termo == 5:
+            print(f'{posicao}', end=' ')
+else:
+    print('O valor 5 não está na lista.')
