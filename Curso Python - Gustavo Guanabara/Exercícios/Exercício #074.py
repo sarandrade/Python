2088a32876aad1d4cc2519658e758293f0e3dafe
sarandrade <sarandradedias@gmail.com>
2020-04-06 12:32:57 -0300

    Correção Ex. 74

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #074.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #074.py
@@ -2,6 +2,18 @@
 
 from random import randint
 
+tupla = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
+
+print('Os valores sorteados foram:', end=" ")
+for i in tupla:
+    print(i, end=' ')
+
+print(f'\nO maior valor sorteado foi: {max(tupla)}')
+print(f'O menor valor sorteado foi: {min(tupla)}')
+
+'''
+=> Ou 
+
 a = randint(0, 10)
 b = randint(0, 10)
 c = randint(0, 10)
@@ -13,3 +25,4 @@ tupla = (a, b, c, d, e)
 print(f'Os valores sorteados foram: {tupla}')
 print(f'O maior valor sorteado foi: {sorted(tupla)[4]}')
 print(f'O menor valor sorteado foi: {sorted(tupla)[0]}')
+'''
