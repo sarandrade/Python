699dd03e3f8cd22e06cfe945b25e6ad23c326932
sarandrade <sarandradedias@gmail.com>
2020-04-07 13:44:29 -0300

    Exercício 85

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #085.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #085.py
@@ -1,1 +1,15 @@
 # Desafios 85
+
+numeros = [[], []]
+
+for i in range(1, 8):
+    num = int(input(f'Digite o {i}° valor: '))
+
+    if num % 2 == 0:
+        numeros[0].append(num)
+    else:
+        numeros[1].append(num)
+
+print('-+-'*10)
+print(f'Valores pares: {sorted(numeros[0])}')
+print(f'Valores ímpares: {sorted(numeros[1])}')
