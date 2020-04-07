7c0dce7d194f4ae7c88057e35b9b19c2c9195989
sarandrade <sarandradedias@gmail.com>
2020-04-07 14:59:37 -0300

    Exercício 88

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #088.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #088.py
@@ -1,1 +1,30 @@
 # Desafios 88
+
+from random import randint
+
+print('-+-'*12)
+print('         JOGA NA MEGA SENHA')
+print('-+-'*12)
+
+num = int(input('Quantos jogos você quer sortear? '))
+
+print(f'\n-=-=- SORTEANDO {num} JOGOS -=-=-')
+
+jogo = []
+
+for j in range(0, num):
+
+    for n in range(0, 6):
+        while True:
+            sort = randint(0, 60)
+            if sort not in jogo:
+                break
+
+        jogo.append(sort)
+
+    print(f'Jogo {j + 1:>2} => {sorted(jogo)}')
+    jogo.clear()
+
+print('-+-'*12)
+print('            BOA SORTE!!')
+print('-+-'*12)
