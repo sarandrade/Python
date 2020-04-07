1fc23b466133dad7236fa08957601333d24e485f
sarandrade <sarandradedias@gmail.com>
2020-04-07 14:01:14 -0300

    Exercício 86

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #086.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #086.py
@@ -1,1 +1,16 @@
 # Desafios 86
+
+matriz = [[], [], []]
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        num = int(input(f'Digite um valor para [{linha}, {coluna}]: '))
+
+        matriz[linha].append(num)
+
+print('-+-'*10)
+
+for linha in matriz:
+    for termo in linha:
+        print(f'[{termo:^4}]', end=' ')
+    print()
