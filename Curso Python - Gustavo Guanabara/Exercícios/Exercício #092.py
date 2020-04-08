9d980983994412175cb291c0751ca5a0ca56ad18
sarandrade <sarandradedias@gmail.com>
2020-04-08 14:43:32 -0300

    Exercício 92

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #092.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #092.py
@@ -1,1 +1,25 @@
 # Desafios 92
+
+from datetime import date
+
+dic = {}
+
+dic['Nome'] = input('Nome: ')
+
+nasc = int(input('Ano de nascimento: '))
+hoje = date.today().year
+idade = hoje - nasc
+dic['Idade'] = idade
+
+dic['CTPS'] = int(input('Carteira de Trabalho (0 não se não tiver): '))
+
+if dic['CTPS'] != 0:
+    cont = int(input('Ano de contratação: '))
+    dic['contratacao'] = cont
+    dic['Aposentadoria'] = idade - (hoje - cont) + 35
+    dic['Salário'] = float(input('Salário: R$'))
+
+print('-+-'*10)
+for k, v in dic.items():
+    print(f'{k} => {v}')
+print('-+-'*10)
