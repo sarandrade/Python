d1c782719aa5cf92c9e9dd3c700bd7cde15a8230
sarandrade <sarandradedias@gmail.com>
2020-04-08 15:03:28 -0300

    Exercício 93

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #093.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #093.py
@@ -1,1 +1,22 @@
 # Desafios 93
+
+dic = {}
+lista = []
+
+dic['nome'] = input('Nome do Jogador: ')
+
+part = int(input(f'Quantas partidas {dic["nome"]} jogou? '))
+
+for i in range(1, part +1):
+    gol = int(input(f'Quantos gols na partida {i}? '))
+    lista.append(gol)
+
+dic['gols'] = lista
+dic['total'] = sum(lista)
+
+print('-+-'*15)
+print(f'O jogador {dic["nome"]} jogou {part} partidas: \n')
+for pos, gol in enumerate(lista):
+    print(f'    ==> Na partida {pos + 1}, fez {gol} gols.')
+print(f'\nFoi um total de {dic["total"]} gols.')
+print('-+-'*15)
