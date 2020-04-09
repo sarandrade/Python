a3992b49037160c662c41a74cfbdbd7440f377ad
sarandrade <sarandradedias@gmail.com>
2020-04-09 20:05:32 -0300

    Exercício 104

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #104.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #104.py
@@ -1,1 +1,15 @@
 # Desafios 104
+
+def leiaint(str):
+    while True:
+        print('-+-'*8)
+        n = input(str).strip()
+
+        if n.isnumeric():
+            return n
+            break
+        else:
+            print('\033[31mErro! Digite um número inteiro!\033[m')
+
+n = leiaint('Digite um número: ')
+print(f'Número digitado: {n}')
