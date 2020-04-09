87ce01ba917739142081545af877a7e203b3ad04
sarandrade <sarandradedias@gmail.com>
2020-04-09 19:54:13 -0300

    Exercício 103

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #103.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #103.py
@@ -1,1 +1,42 @@
 # Desafios 103
+
+def ficha(jog='<desconhecido>', gol=0):
+    print(f'O jogador {jog} fez {gol} gol(s)')
+
+print('-+-'*20)
+nome = input('Nome do jogador: ')
+gol = input('Número de gols: ')
+
+if gol.isnumeric():
+    gol = int(gol)
+else:
+    gol = 0
+
+if nome.strip() == '':
+    ficha(gol=gol)
+else:
+    ficha(nome, gol)
+
+'''
+==> Como eu fiz...
+
+def testenome(nome):
+    if nome.strip() != '':
+        return nome
+    else:
+        return '<desconhecido>'
+
+def testegol(gols):
+    if gols != '' and gols.isnumeric():
+        return gols
+    else:
+        return 0
+
+nome = ''
+gols = ''
+print('-+-'*20)
+nome = input('Nome do jogador: ')
+gols = input('Número de gols: ')
+
+print(f'O jogador {testenome(nome)} fez {testegol(gols)} gol(s).')
+'''
