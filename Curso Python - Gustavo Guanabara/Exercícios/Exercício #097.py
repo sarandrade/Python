52d541070a93c143c7b2c94578635e9f2a4b47fc
sarandrade <sarandradedias@gmail.com>
2020-04-09 09:54:54 -0300

    Exercício 97

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #097.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #097.py
@@ -1,1 +1,11 @@
 # Desafios 97
+
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~'*tam)
+    print(f'  {msg} ')
+    print('~'*tam)
+
+escreva('Oi')
+escreva('Olá, Mundo!')
+escreva('Sara Andrade Dias')
