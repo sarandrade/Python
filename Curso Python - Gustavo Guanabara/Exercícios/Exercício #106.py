4f65766e103c0b2a13fade870b2da6045f39993d
sarandrade <sarandradedias@gmail.com>
2020-04-09 21:11:10 -0300

    Exercício 106

--- Curso Python - Gustavo Guanabara/Exercícios/Exercício #106.py
+++ Curso Python - Gustavo Guanabara/Exercícios/Exercício #106.py
@@ -1,1 +1,23 @@
 # Desafios 106
+
+def alinhar(str):
+    tam = len(str) + 4
+    print('~'*tam)
+    print(f'  {str}')
+    print('~'*tam)
+
+def ajuda(opc):
+    print()
+    alinhar(f'Acessando o manual do comando {opc}...')
+    help(opc)
+
+opc = ''
+while opc.upper() != 'FIM':
+    alinhar('SISTEMA DE AJUDA')
+
+    opc = input('Função ou Biblioteca > ')
+    
+    if opc.upper() != 'FIM':
+        ajuda(opc)
+
+alinhar('ATÉ LOGO!')
