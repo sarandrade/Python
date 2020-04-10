6640697dcbc0304e70781867623a964b1080d4df
sarandrade <sarandradedias@gmail.com>
2020-04-10 10:09:18 -0300

    Exercício 113

--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #113.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #113.py
@@ -1,1 +1,33 @@
 # Desafios 113
+
+def leiaint(str):
+    while True:
+        try:
+            num = int(input(str))
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        except:
+            print(f'\033[31mERRO: Por favor, digite um número inteiro válido!\033[m')
+            continue
+        else:
+            return num
+
+def leiafloat(str):
+    while True:
+        try:
+            num = float(input(str))
+        except (KeyboardInterrupt):
+            print('\n\033[31mEntrada de dados interrompida pelo usuário.\033[m')
+            return 0
+        except:
+            print(f'\033[31mERRO: Por favor, digite um número real válido!\033[m')
+            continue
+        else:
+            return num
+
+print('-+-'*10)
+num = leiaint('Digite um número inteiro: ')
+flo = leiafloat('Digite um número real: ')
+print(f'Os números digitados foram {num} e {flo}')
+print('-+-'*10)
