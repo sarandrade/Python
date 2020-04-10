d9a3595d2ce9799f7b1f0a054bf27bb09d0db191
sarandrade <sarandradedias@gmail.com>
2020-04-10 07:24:09 -0300

    Exercício 109

--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #109.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #109.py
@@ -1,1 +1,10 @@
 # Desafios 109
+
+from moeda import * # Importa tudo de moeda
+
+num = float(input('Digite um valor: R$  '))
+
+print(f'A metade de {moeda(num)} é {metade(num, True)}.')
+print(f'O dobro de {moeda(num)} é {dobro(num, True)}.')
+print(f'Aumentando em 10%, temos {aumentar(num, 10, True)}.')
+print(f'Reduzindo em 13%, temos {diminuir(num, 13, True)}.')
--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/moeda.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/moeda.py
@@ -1,16 +1,28 @@
 # Módulo do Exercício #107
 
-def metade(num=0):
-    return num / 2
+def metade(num=0, sit=False):
+    if sit:
+        return moeda(num / 2)
+    else:
+        return num / 2
 
-def dobro(num=0):
-    return num * 2
+def dobro(num=0, sit=False):
+    if sit:
+        return moeda(num * 2)
+    else:
+        return num * 2
 
-def aumentar(num=0, p=0):
-    return num * (1 + p/100)
+def aumentar(num=0, p=0, sit=False):
+    if sit:
+        return moeda(num * (1 + p/100))
+    else:
+        return num * (1 + p/100)
 
-def diminuir(num=0, p=0):
-    return num * (1 - p/100)
+def diminuir(num=0, p=0, sit=False):
+    if sit:
+        return moeda(num * (1 - p/100))
+    else:
+        return num * (1 - p/100)
 
 def moeda(num=0, moeda='R$'):
     return f"{moeda} {num:.2f}".replace('.',',')
