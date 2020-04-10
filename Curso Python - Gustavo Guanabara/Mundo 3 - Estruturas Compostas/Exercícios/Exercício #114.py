a6ab3eb9d9fb4ba1ca6665d7c3916a6f9979c290
sarandrade <sarandradedias@gmail.com>
2020-04-10 10:37:51 -0300

    Exercício 114

--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #114.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #114.py
@@ -1,1 +1,11 @@
 # Desafios 114
+
+import requests
+
+try:
+    r = requests.get('http://pudim.com.br/')
+except:
+    print('\033[31mO site Pudim não está acessível no momente.\033[m')
+else:
+    print('\033[32mO site Pudim está acessível no momente.\033[m')
+    print(r.text)
