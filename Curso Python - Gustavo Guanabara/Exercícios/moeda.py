b4370755b6c0422670b725aefab44412b3a068c9
sarandrade <sarandradedias@gmail.com>
2020-04-10 06:21:29 -0300

    Exercício 107

---
+++ Curso Python - Gustavo Guanabara/Exercícios/moeda.py
@@ -0,0 +1,13 @@
+# Módulo do Exercício #107
+
+def metade(num):
+    return num / 2
+
+def dobro(num):
+    return num * 2
+
+def aumentar(num, p):
+    return num * (1 + p/100)
+
+def diminuir(num, p):
+    return num * (1 - p/100)
