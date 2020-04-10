88130ad2c2fff00818572b73e0ac24509c51a758
sarandrade <sarandradedias@gmail.com>
2020-04-10 07:42:51 -0300

    Exercício 110

--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #110.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/Exercício #110.py
@@ -1,1 +1,7 @@
 # Desafios 110
+
+from moeda import * # Importa tudo de moeda
+
+num = float(input('Digite um valor: R$  '))
+
+resumo(num, 90, 25)
--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/moeda.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Exercícios/moeda.py
@@ -26,3 +26,14 @@ def diminuir(num=0, p=0, sit=False):
 
 def moeda(num=0, moeda='R$'):
     return f"{moeda} {num:.2f}".replace('.',',')
+
+def resumo(num=0, tx1=0, tx2=0):
+    print('-'*35)
+    print('    RESUMO DO VALOR')
+    print('-'*35)
+    print(f'{"Preço analisado:":<20}{moeda(num):>15}')
+    print(f'{"Dobro do preço:":<20}{dobro(num, True):>15}')
+    print(f'{"Metade do preço:":<20}{metade(num, True):>15}')
+    print(f'{tx1:>2}{"% de aumento:":<18}{aumentar(num, tx1, True):>15}')
+    print(f'{tx2:>2}{"% de redução:":<18}{diminuir(num, tx2, True):>15}')
+    print('-'*35)
