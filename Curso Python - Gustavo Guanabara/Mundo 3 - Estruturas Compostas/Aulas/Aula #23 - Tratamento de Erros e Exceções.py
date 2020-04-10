53b9fc5250d2f4152d977769ce55a3e1b2608b89
sarandrade <sarandradedias@gmail.com>
2020-04-10 09:22:14 -0300

    Aula 23

--- Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Aulas/Aula #23 - Tratamento de Erros e Exceções.py
+++ Curso Python - Gustavo Guanabara/Mundo 3 - Estruturas Compostas/Aulas/Aula #23 - Tratamento de Erros e Exceções.py
@@ -1,1 +1,44 @@
 # Aula #23 - Tratamento de Erros e Exceções
+
+'''
+try:
+    operação
+except:
+    falhou
+else:
+    deu certo
+finally:
+    certo ou errado
+'''
+
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except Exception as erro:
+    print('Infelizmente houve um problema :(')
+    print(f'O problema encontrado foi {erro.__class__}')
+else:
+    print(f'O resultado é {r:.2f}')
+finally:
+    print('Volte sempre! Muito obrigada!')
+
+
+try:
+    a = int(input('Numerador: '))
+    b = int(input('Denominador: '))
+    r = a / b
+except (ValueError, TypeError):
+    print('Tivemos um problema com os tipos de dados que você digitou.')
+except ZeroDivisionError:
+    print('Não é possível dividir um número por zero.')
+except KeyboardInterrupt:
+    print('O usuário não informou os dados.')
+except Exception as erro:
+    print(f'O erro encontrado foi {erro.__class__}')
+else:
+    print(f'O resultado é {r:.2f}')
+finally:
+    print('Volte sempre! Muito obrigada!')
+
+# Desafios 113 -> 115
