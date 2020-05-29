0afbfb6a8c1f946a36164ce12f7da0d354cc2985
sarandrade <sarandradedias@gmail.com>
2020-05-29 11:23:33 -0300

    Tipos Básicos

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 5 - Fundamentos Python/Tipos Basicos.py
@@ -0,0 +1,36 @@
+# Tipo Boolean - bool
+
+print(type(True))
+print(type(False))
+
+# Tipo Inteiro - int
+
+print(type(5))
+
+# Tipo Real - float
+
+print(type(2.5))
+
+# Tipo String - str
+
+print(type('Sara'))
+print(type("Sara"))
+
+# Lista
+
+lista = [1, 2, 3]
+print(lista)
+
+# Dicionário
+
+dic = {'nome':'Sara', 'idade':21}
+print(dic)
+
+# Tipo None
+
+print(type(None))
+
+# Misturando tipos
+
+print('Você é ' + 3 * 'muito ' + 'legal!') 
+# print(3 + '3') -> Erro: ambíguidade 
