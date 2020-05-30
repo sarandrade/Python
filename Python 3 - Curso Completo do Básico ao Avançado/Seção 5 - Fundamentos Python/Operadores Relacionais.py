e2c0afba0f512b111f1f33738807f871dac95101
sarandrade <sarandradedias@gmail.com>
2020-05-30 12:11:17 -0300

    Operadores de Atribuição

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 5 - Fundamentos Python/Operadores Relacionais.py
@@ -0,0 +1,18 @@
+# Maior que...
+print(3 > 4)
+
+# Maior ou igual a...
+print(3 >= 5)
+
+# Menor que...
+print(2 < 5)
+
+# Menor ou igual a...
+print(5 <= 2)
+
+# Diferente 
+print(3 != 2)
+
+# Igual 
+print(2 == 2)
+print(2 == '2') # São tipos diferentes
---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 5 - Fundamentos Python/Operadores de Atribuição.py
@@ -0,0 +1,26 @@
+a = 5 
+print(a)
+
+a = a + 2
+print(a)
+
+a += 5
+print(a)
+
+a -= 2
+print(a)
+
+a *= 2
+print(a)
+
+a /= 4
+print(a)
+
+a %= 4
+print(a)
+
+a **= 8
+print(a)
+
+a //= 0.2
+print(a)
