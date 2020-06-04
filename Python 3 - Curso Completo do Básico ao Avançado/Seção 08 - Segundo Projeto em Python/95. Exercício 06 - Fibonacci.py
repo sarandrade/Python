f07347110d7c787efbbf5a0fbadcb3e65d0d36c7
sarandrade <sarandradedias@gmail.com>
2020-06-04 14:16:35 -0300

    Exercício Fibinacci 06

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 08 - Segundo Projeto em Python/95. Exercício 06 - Fibonacci.py
@@ -0,0 +1,15 @@
+# Sequência de Fibinacci utilizando recursividade
+# 0, 1, 1, 2, 3, 5, 8, 13...
+
+def fibonacci(quantidade, resultado=(0, 1)):
+    if len(resultado) == quantidade:
+        return resultado
+
+    return fibonacci(quantidade, resultado + (sum(resultado[-2:]),))
+
+
+
+if __name__ == '__main__':
+    # Lista os 15 primeiros números da sequência
+    for valor in fibonacci(15):
+        print(valor, end=', ')
