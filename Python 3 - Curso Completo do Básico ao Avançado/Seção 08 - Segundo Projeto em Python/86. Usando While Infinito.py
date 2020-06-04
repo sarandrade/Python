ce9b2d1b8338b4d72cbc3f9ccd4cb04b6ab36358
sarandrade <sarandradedias@gmail.com>
2020-06-04 13:08:17 -0300

    Exercício while 01

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 08 - Segundo Projeto em Python/86. Usando While Infinito.py
@@ -0,0 +1,15 @@
+# Sequência de Fibinacci
+# 0, 1, 1, 2, 3, 5, 8, 13...
+
+def fibonacci(limite):
+    primeiro = 0
+    segundo = 1
+    proximo = 0
+    print(f'{primeiro}, {segundo}', end=', ')
+    # whilw True: -> While infinito
+    while segundo < limite:
+        primeiro, segundo = segundo, primeiro + segundo
+        print(f'{segundo}', end=', ')
+
+if __name__ == '__main__':
+    fibonacci(150)
