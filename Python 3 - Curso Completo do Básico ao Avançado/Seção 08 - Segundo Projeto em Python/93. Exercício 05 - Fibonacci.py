96ab5f7e762c2eb04ec642753d5cfd133615132e
sarandrade <sarandradedias@gmail.com>
2020-06-04 13:54:29 -0300

    Exercício Fibinacci 05

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 08 - Segundo Projeto em Python/93. Exercício 05 - Fibonacci.py
@@ -0,0 +1,15 @@
+# Sequência de Fibinacci utilizando a função sum()
+# 0, 1, 1, 2, 3, 5, 8, 13...
+
+def fibonacci(quantidade):
+    resultado = [0, 1]
+    # '_' -> Variável não é usada 
+    for _ in range(quantidade - 2):
+        resultado.append(sum(resultado[-2:]))
+
+    return resultado
+
+if __name__ == '__main__':
+    # Lista os 15 primeiros números da sequência
+    for valor in fibonacci(15):
+        print(valor, end=', ')
