409a3eb80107a25560b390facab266427957ea56
sarandrade <sarandradedias@gmail.com>
2020-06-11 17:04:05 -0300

    Visão Geral da POO

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 13 - Programação Orientada a Objeto/140. Visão Geral da POO.py
@@ -0,0 +1,16 @@
+'''
+Membros de um Objeto
+    -> Atributos - dados
+    -> Comportamentos - métodos (funções)
+
+Classe    vs   Objeto
+Molde(1)  vs   Intância(n)
+
+- A partir de uma classe é possível criar uma estrutura de dados
+
+Pilares da Orientação a Objeto
+    -> Herança
+    -> Polimorfismo
+    -> Encapsulamento
+    -> Abstração
+'''
