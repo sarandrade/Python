a4a3d07192a80a22a87953ebd4b69f1186580add
sarandrade <sarandradedias@gmail.com>
2020-06-14 10:38:23 -0300

    Propriedades_01 (get e set)

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 14 - Programação Orientada a Objeto Avançada/160. Propriedades_01 (get e set).py
@@ -0,0 +1,51 @@
+class Humano:
+    # Atributo de classe
+    especie = 'Homo Sapiens'
+
+
+    def __init__(self, nome):
+        self.nome = nome
+        self._idade = None
+
+
+    def get_idade(self):
+        return self._idade
+
+
+    def set_idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade deve ser um número positivo!')
+
+        self._idade = idade
+
+    # Método de Instância
+    def das_cavernas(self):
+        # Atributo de instância
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    # Método Estático
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    # Método de Classe
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class HomoNeanderthalensis(Humano):
+    especie = Humano.especies()[-2]
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == "__main__":
+    jose = HomoSapiens('José')
+    jose.set_idade(40)
+    print(f'Nome: {jose.nome} | Idade: {jose.get_idade()}')
