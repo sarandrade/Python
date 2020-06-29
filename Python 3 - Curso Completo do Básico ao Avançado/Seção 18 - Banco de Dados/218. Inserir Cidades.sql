16db27a84751b37fabc10d527123dfb0e4e2a965
sarandrade <sarandradedias@gmail.com>
2020-06-29 14:23:04 -0300

    Banco de dados - JOIN

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 18 - Banco de Dados/218. Inserir Cidades.sql
@@ -0,0 +1,22 @@
+insert into cidades 
+    (nome, area, estado_id)
+values
+    ('Niterói', 133.9, 46)
+
+insert into cidades
+    (nome, area, estado_id)
+values (
+    'Caruaru', 
+    920.6, 
+    (select id from estados where sigla = 'PE')
+)
+
+insert into cidades
+    (nome, area, estado_id)
+values (
+    'Juazeiro do Norte', 
+    248.2, 
+    (select id from estados where sigla = 'CE')
+)
+
+select * from cidades
