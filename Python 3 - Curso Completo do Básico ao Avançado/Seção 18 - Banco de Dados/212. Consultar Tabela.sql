580cfc9a1ebf9e6a51115b143c789dda0a57f13d
sarandrade <sarandradedias@gmail.com>
2020-06-29 12:04:41 -0300

    Banco de dados_01

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 18 - Banco de Dados/212. Consultar Tabela.sql
@@ -0,0 +1,19 @@
+select * from estados
+
+select 
+    Sigla, 
+    nome as 'Nome do Estado' 
+from estados
+where regiao = 'Sul'
+
+select nome, regiao, populacao from estados
+where populacao >= 10
+order by populacao -- Forma crescente
+
+select 
+    nome, 
+    regiao, 
+    populacao 
+from estados
+where populacao >= 10
+order by populacao desc -- Forma decrescente
---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 18 - Banco de Dados/214. Inserir Estados com ID.sql
@@ -0,0 +1,12 @@
+insert into estados
+    (id, nome, sigla, regiao, populacao)
+values
+    (1000, 'Novo', 'NV', 'Sul', 2.54),
+    (999, 'Mais Novo', 'MN', 'Norte', 2.51)
+
+insert into estados
+    (nome, sigla, regiao, populacao)
+values
+    ('Ainda mais novo', 'AN', 'Norte', 2.51)
+
+select * from estados
