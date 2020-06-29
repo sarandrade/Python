734ade023ef56b489d4609526ea871d1ec10f7f8
sarandrade <sarandradedias@gmail.com>
2020-06-29 14:59:27 -0300

    Banco de dados - Tipos de JOINs

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 18 - Banco de Dados/223. Explorando os Tipos de JOINs.sql
@@ -0,0 +1,26 @@
+-- Rodar código no MySQL
+
+select * from cidades c 
+inner join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c 
+left outer join prefeitos p
+on c.id = p.cidade_id;
+
+select * from cidades c 
+right join prefeitos p
+on c.id = p.cidade_id;
+
+-- select * from cidades c 
+-- full join prefeitos p
+-- on c.id = p.cidade_id;
+
+-- Equivalente ao full join em MySQL
+select * from cidades c 
+left outer join prefeitos p
+on c.id = p.cidade_id
+union
+select * from cidades c 
+right join prefeitos p
+on c.id = p.cidade_id;
