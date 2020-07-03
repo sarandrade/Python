3f13f5a66c8394e2b58d87fb7c5a5338930692e5
sarandrade <sarandradedias@gmail.com>
2020-07-03 11:29:29 -0300

    Consultar com Input

---
+++ Python 3 - Curso Completo do Básico ao Avançado/Seção 19 - Banco de Dados com Python/248. Consultar com Input.py
@@ -0,0 +1,13 @@
+from conexao import nova_conexao
+
+sql = 'SELECT nome, tel FROM contatos WHERE nome like %s'
+
+with nova_conexao() as conexao:
+    nome = input('Contato a localizar: ')
+    args = (f'%{nome}%', )
+
+    cursor = conexao.cursor()
+    cursor.execute(sql, args)
+
+    for x in cursor:
+        print(x)
